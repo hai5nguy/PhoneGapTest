8177e9bd191fe1be9f2c306b2d8e78522bbe36fb
Hai Nguyen <nmhai3@gmail.com>
2013-06-05 16:20:46 -0400

    initial

---
+++ PhoneGapTest.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    $("#btnCamera").click(function () {
+        alert("hello");
+        navigator.camera.getPicture(
+            function () { alert("success"); },
+            function () { alert("failed"); },
+            { quality: 50, destinationType: Camera.DestinationType.DATA_URL }
+        );
+
+    });
+});
